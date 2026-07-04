5bcf649136ddcb7ff35f03023b4c39d9c57e9802
barisic-durdica <barisic.djurdjica@gmail.com>
2026-07-04 22:47:03 +0100

    wk1_exercise_4

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,68 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+
+        int number = -1;
+
+        while (number != 0)
+        {
+            Console.Write("Enter number: ");
+            number = int.Parse(Console.ReadLine());
+
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }
+        }
+
+        int sum = 0;
+
+        foreach (int item in numbers)
+        {
+            sum += item;
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
+
+        double average = (double)sum / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        int largest = numbers[0];
+
+        foreach (int item in numbers)
+        {
+            if (item > largest)
+            {
+                largest = item;
+            }
+        }
+
+        Console.WriteLine($"The largest number is: {largest}");
+
+        int smallestPositive = int.MaxValue;
+
+        foreach (int item in numbers)
+        {
+            if (item > 0 && item < smallestPositive)
+            {
+                smallestPositive = item;
+            }
+        }
+
+        Console.WriteLine($"The smallest positive number is: {smallestPositive}");
+
+        numbers.Sort();
+
+        Console.WriteLine("The sorted list is:");
+
+        foreach (int item in numbers)
+        {
+            Console.WriteLine(item);
+        }
     }
 }
